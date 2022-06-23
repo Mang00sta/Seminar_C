237d076d6ab9bd761ddf93e1e4381e259e4d965f
Vishnyakov <mangostalife@gmail.com>
2022-06-23 23:30:19 +0300

    закомитил

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,100 +1,104 @@
 ﻿// 1я задача
-// int number;
+/*
+int number;
 
-// Console.Write("Input a number: ");
-// number = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Input a number: ");
+number = Convert.ToInt32(Console.ReadLine()); 
 
-// Console.WriteLine("Your number is" + number);
+Console.WriteLine("Your number is" + number);
+*/
 
 // 2я задача
+/*
+int num1, num2, quad;
 
-// int num1, num2, quad;
+Console.Write("Input a number: ");
+num1 = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Input a second: ");
+num2 = Convert.ToInt32(Console.ReadLine()); 
 
-// Console.Write("Input a number: ");
-// num1 = Convert.ToInt32(Console.ReadLine()); 
-// Console.Write("Input a second: ");
-// num2 = Convert.ToInt32(Console.ReadLine()); 
+quad=num2*num2;
 
-// quad=num2*num2;
+    if(quad == num1) 
+{
+        Console.Write("First num is quad og second ");
+}
+    else
+{
+        Console.Write("Second num is quad og second ");
+}
+*/
 
-//     if(quad == num1) 
-// {
-//         Console.Write("First num is quad og second ");
-// }
-//     else
-// {
-//         Console.Write("Second num is quad og second ");
-// }
+//Задача 3
+/*
+int day;
 
-// Задача 3
+Console.Write("Input a day number: ");
 
-// int day;
+day = Convert.ToInt32(Console.ReadLine()); 
 
-// Console.Write("Input a day number: ");
-
-// day = Convert.ToInt32(Console.ReadLine()); 
-
-//     if(day == 1)
-// {
-//     Console.WriteLine ("Mon");
+    if(day == 1)
+{
+    Console.WriteLine ("Mon");
     
-// }
+}
 
-//     if(day == 2)
+    if(day == 2)
 
-// {
-//     Console.WriteLine ("Thu");
+{
+    Console.WriteLine ("Thu");
     
-// }
+}
 
-//     if(day == 3)
+    if(day == 3)
 
-// {
-//     Console.WriteLine ("Wen");
+{
+    Console.WriteLine ("Wen");
     
-// }
-//     if(day == 4)
+}
+    if(day == 4)
 
-// {
-//     Console.WriteLine ("Thu");
+{
+    Console.WriteLine ("Thu");
     
-// }
-//     if(day == 5)
+}
+    if(day == 5)
 
-// {
-//     Console.WriteLine ("Fri");
+{
+    Console.WriteLine ("Fri");
     
-// }
-//     if(day == 6)
+}
+    if(day == 6)
 
-// {
-//     Console.WriteLine ("Sat");
+{
+    Console.WriteLine ("Sat");
     
-// }
-//     if(day == 7)
+}
+    if(day == 7)
 
-// {
-//     Console.WriteLine ("Sun");
+{
+    Console.WriteLine ("Sun");
     
-// }
-//     else
-// {
-//         Console.Write("Wrong number");
-// }
-
+}
+    else
+{
+        Console.Write("Wrong number");
+}
+*/
 // Задача 4
+/*
+int num1, quad;
 
-// int num1, quad;
+Console.Write("Input a number: ");
+num1 = Convert.ToInt32(Console.ReadLine()); 
 
-// Console.Write("Input a number: ");
-// num1 = Convert.ToInt32(Console.ReadLine()); 
+quad=num1*num1;
 
-// quad=num1*num1;
-
-// Console.Write("Your quad number " + quad);
+Console.Write("Your quad number " + quad);
+*/
 
 // Задача 5
-
+/*
 int n, current;
 
 Console.Write("input a num ");
@@ -107,3 +111,4 @@ while(current <= n)
     Console.Write(current + " ");
     current++; //current = current + 1
 }
+*/
