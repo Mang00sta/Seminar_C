632eb5c76cd6bedd751a0808b41444188e02c58a
Vishnyakov <mangostalife@gmail.com>
2022-06-23 20:54:54 +0300

    Задача 2

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1я задача
+// int number;
+
+// Console.Write("Input a number: ");
+// number = Convert.ToInt32(Console.ReadLine()); 
+
+// Console.WriteLine("Your number is" + number);
+
+// 2я задача
+
+// int num1, num2, quad;
+
+// Console.Write("Input a number: ");
+// num1 = Convert.ToInt32(Console.ReadLine()); 
+// Console.Write("Input a second: ");
+// num2 = Convert.ToInt32(Console.ReadLine()); 
+
+// quad=num2*num2;
+
+//     if(quad == num1) 
+// {
+//         Console.Write("First num is quad og second ");
+// }
+//     else
+// {
+//         Console.Write("Second num is quad og second ");
+// }
