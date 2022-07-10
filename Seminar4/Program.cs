e24503d1b323ba11008d1acc9e7a844187d3ab08
Vishnyakov <mangostalife@gmail.com>
2022-07-10 20:07:13 +0300

    мелочи

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -35,11 +35,11 @@ void ShowArray (int[] array) // метод подбора
     
 }
 
-int FindPositivSum(int[] array);
+int FindPositivSum(int[] array)
 {
     int sum=0;
 
-    for (int i=0; i<array.Lenght; i++)
+    for (int i=0; i<array.Length; i++)
         if (array[i]>0) sum+=array[i];
 
     return sum;
